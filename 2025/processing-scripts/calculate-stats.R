25ce1d4bff258345fa7661f50468a222e9dd1965
jdjohn215 <jdjohn215@gmail.com>
2025-04-09 21:19:13 -0500

    add 2025 workflow and preliminary data

---
+++ 2025/processing-scripts/calculate-stats.R
@@ -0,0 +1,114 @@
+rm(list = ls())
+
+library(tidyverse)
+
+possession.df <- read_csv("2025/integ-data/Possessions.csv")
+
+# this is per-game player statistics computed by Statto
+player.stats.df <- read_csv("2025/integ-data/Player-Stats.csv") |>
+  # convert meters to yards
+  mutate(across(.cols = contains("(m)"), .fn = ~.x * 1.094)) |>
+  rename_with(.fn = \(x)sub("[(]m[)]","(yd)",x))
+
+defensive.blocks.df <- read_csv("2025/integ-data/Defensive-Blocks.csv")
+
+points.df <- read_csv("2025/integ-data/Points.csv")
+
+# this is 1-row per pass
+##  use it to calculate the OVERALL pass and reception average distance
+passes.df <- read_csv("2025/integ-data/Passes.csv") |>
+  # convert meters to yards
+  mutate(across(.cols = contains("(m)"), .fn = ~.x * 1.094)) |>
+  rename_with(.fn = \(x)sub("[(]m[)]","(yd)",x))
+
+stat.priority <- read_csv("PUL Stats Hub Priority.csv")
+
+################################################################################
+# Team stats
+stat.priority  |> filter(`Priority Tier` == "1 - Must Have", Type == "Team")
+stat.priority  |> filter(`Priority Tier` == "2 - Nice To Have", Type == "Team")
+
+team.goal.stats.overall <- points.df |>
+  group_by(team) |>
+  summarise(goals = sum(`Scored?`),
+            break_goals = sum(`Scored?`[`Started on offense?` == 0]),
+            defensive_blocks = sum(`Defensive blocks`),
+            holds = sum(`Started on offense?` == 1 & `Scored?` == 1),
+            clean_holds = sum(`Scored?` == 1 &
+                                `Started on offense?` == 1 &
+                                Turnovers == 0))
+team.pass.stats.overall <- passes.df |>
+  group_by(team) |>
+  summarise(pass_attempts = n(),
+            turnovers = sum(`Turnover?`),
+            completed_passess = sum(`Turnover?` == 0),
+            completion_rate = (completed_passess/pass_attempts),
+            hucks = sum(`Huck?`))
+
+team.goal.stats.game <- points.df |>
+  group_by(team, match, week) |>
+  summarise(goals = sum(`Scored?`),
+            break_goals = sum(`Scored?`[`Started on offense?` == 0]),
+            defensive_blocks = sum(`Defensive blocks`),
+            holds = sum(`Started on offense?` & `Scored?` == 1),
+            clean_holds = sum(`Scored?` == 1 &
+                                `Started on offense?` == 1 &
+                                Turnovers == 0),
+            .groups = "drop")
+team.pass.stats.game <- passes.df |>
+  group_by(team, match, week) |>
+  summarise(pass_attempts = n(),
+            turnovers = sum(`Turnover?`),
+            completed_passess = sum(`Turnover?` == 0),
+            completion_rate = (completed_passess/pass_attempts),
+            hucks = sum(`Huck?`),
+            .groups = "drop")
+
+team.stats.overall <- inner_join(team.goal.stats.overall, team.pass.stats.overall)
+team.stats.game <- inner_join(team.goal.stats.game, team.pass.stats.game)
+
+################################################################################
+# Individual stats
+stat.priority  |> filter(`Priority Tier` == "1 - Must Have", Type == "Individual")
+stat.priority  |> filter(`Priority Tier` == "2 - Nice To Have", Type == "Individual")
+
+
+player.stats.game <- player.stats.df |>
+  select(week, match, team, Player, Touches, Throws, Catches, `Defensive blocks`,
+         Goals, Turnovers, `Total completed throw gain (yd)`, `Average completed throw gain (yd)`,
+         `Total caught pass gain (yd)`, `Average caught pass gain (yd)`,
+         `Offense points played`, `Defense points played`, `Possessions initiated`,
+         `Thrower errors`, `Receiver errors`, Assists, `Secondary assists`)
+
+overall.pass.dist <- passes.df |>
+  filter(`Turnover?` == 0) |>
+  group_by(team, Player = Thrower) |>
+  summarise(`Average completed throw gain (yd)` = mean(`Forward distance (yd)`))
+
+overall.receive.dist <- passes.df |>
+  filter(`Turnover?` == 0) |>
+  group_by(team, Player = Receiver) |>
+  summarise(`Average caught pass gain (yd)` = mean(`Forward distance (yd)`))
+
+player.stats.overall <- player.stats.game |>
+  select(-contains("Average")) |> # remove averages which can't be summed
+  # aggregate
+  group_by(Player, team) |>
+  summarise(across(.cols = where(is.numeric), .fn = sum),
+            .groups = "drop") |>
+  # add overall pass & reception averages
+  left_join(overall.pass.dist) |>
+  left_join(overall.receive.dist) |>
+  select(team, Player, Touches, Throws, Catches, `Defensive blocks`,
+         Goals, Turnovers, `Total completed throw gain (yd)`, `Average completed throw gain (yd)`,
+         `Total caught pass gain (yd)`, `Average caught pass gain (yd)`,
+         `Offense points played`, `Defense points played`, `Possessions initiated`,
+         `Thrower errors`, `Receiver errors`, Assists, `Secondary assists`)
+
+
+################################################################################
+# save stats
+write_csv(team.stats.overall, "2025/stats/team-stats-overall.csv")
+write_csv(team.stats.game, "2025/stats/team-stats-game.csv")
+write_csv(player.stats.overall, "2025/stats/player-stats-overall.csv")
+write_csv(player.stats.game, "2025/stats/player-stats-game.csv")
---
+++ 2025/processing-scripts/integrate-raw-game-data.R
@@ -0,0 +1,87 @@
+rm(list = ls())
+
+library(tidyverse)
+
+################################################################################
+# This script extracts the raw statistics files and integrates them
+#   It also creates a file summarizing game coverage.
+
+################################################################################
+# Step 1: list all the CSV files in the raw game stats directory
+#   I download this entire folder through the web interface to google drive
+all.files <- list.files("2025/2025 Game Day Info/", pattern = ".csv",
+                        full.names = T, recursive = T)
+
+# remove the "original stats" subdirectories
+all.files <- all.files[str_detect(all.files, "original stats", negate = T)]
+
+# this function reads a given path
+#   it adds match info to the file
+read_stat <- function(path){
+  path
+  match = str_extract(path, "(?<before>\\w+) @ (?<after>\\w+)") # teams
+  # this regex could be improved
+  match.week <- str_extract(path, "2025 Game Day Info/(.*?)_") |> str_remove("2025 Game Day Info/") |> str_remove("_") |> str_remove(coll("/"))
+  team = word(path, -2, sep = "/") # this team these stats apply to
+  opponent = str_remove_all(match, paste(team, "@", " ", sep = "|"))
+  
+  read_csv(path, col_types = cols(.default = "c")) |>
+    mutate(match = match,
+           week = match.week,
+           team = team,
+           opponent = opponent) |>
+    select(match, week, team, opponent, everything())
+}
+
+################################################################################
+# Step 2: extract each statistic type
+possession.df <- map_df(.x = all.files[which(str_detect(all.files, "Possession"))],
+                        .f = read_stat)
+player.stats.df <- map_df(.x = all.files[which(str_detect(all.files, "Player Stats vs"))],
+                        .f = read_stat)
+defensive.blocks.df <- map_df(.x = all.files[which(str_detect(all.files, "Defensive Blocks|Defensive_Blocks"))],
+                          .f = read_stat)
+points.df <- map_df(.x = all.files[which(str_detect(all.files, "Points vs"))],
+                          .f = read_stat)
+passes.df <- map_df(.x = all.files[which(str_detect(all.files, "Passes vs"))],
+                    .f = read_stat)
+
+################################################################################
+# Step 3: # save to the integ-data folder
+write_csv(possession.df, "2025/integ-data/Possessions.csv")
+write_csv(player.stats.df, "2025/integ-data/Player-Stats.csv")
+write_csv(defensive.blocks.df, "2025/integ-data/Defensive-Blocks.csv")
+write_csv(points.df, "2025/integ-data/Points.csv")
+write_csv(passes.df, "2025/integ-data/Passes.csv")
+
+################################################################################
+# Step 4: Check for data coverage
+
+# Is each coverage element available?
+data.element.coverage <- possession.df |> group_by(match, week, team) |> summarise(possessions = T, .groups = "drop") |>
+  full_join(player.stats.df |> group_by(match, week, team) |> summarise(player_stats = T, .groups = "drop")) |>
+  full_join(defensive.blocks.df |> group_by(match, week, team) |> summarise(defensive_blocks = T, .groups = "drop")) |>
+  full_join(points.df |> group_by(match, week, team) |> summarise(points = T, .groups = "drop")) |>
+  full_join(passes.df |> group_by(match, week, team) |> summarise(passes = T, .groups = "drop")) |>
+  mutate(home_away = if_else(team == word(match, 1), "away", "home"),
+         # convert NAs to FALSE (designating missing data)
+         across(.cols = where(is.logical), .fns = ~ifelse(is.na(.x), FALSE, TRUE))) |>
+  select(match, week, home_away, team, everything())
+
+# which teams play each game?
+teams.per.match <- data.element.coverage |>
+  select(match, week) |>
+  separate(match, into = c("away","home"), sep = " @ ", remove = F) |>
+  pivot_longer(cols = c(away, home), names_to = "home_away", values_to = "team") |>
+  group_by_all() |>
+  summarise(.groups = "drop")
+
+# is each team accounted for?
+game.coverage <- data.element.coverage |>
+  full_join(teams.per.match) |>
+  # convert NAs to FALSE (designating missing data)
+  mutate(across(.cols = where(is.logical), .fns = ~ifelse(is.na(.x), FALSE, TRUE))) |>
+  arrange(week, match, home_away)
+
+write_csv(game.coverage, "2025/stats/game-coverage.csv")
+
